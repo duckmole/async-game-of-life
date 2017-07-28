c23d00486d1bf7ebf3d29e6a5359d898957c139d
Fabrice Nourisson <fnourisson@servebox.com>
2017-07-28 15:05:29 +0200

    Initialize project with `mix new`

---
+++ lib/async_game_of_life.ex
@@ -0,0 +1,18 @@
+defmodule AsyncGameOfLife do
+  @moduledoc """
+  Documentation for AsyncGameOfLife.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> AsyncGameOfLife.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
